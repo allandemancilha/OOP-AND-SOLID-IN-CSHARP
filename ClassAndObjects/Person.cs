a2b2d4e8a6876053c9c295610fd32b0c0c445d63
allandemancilha <allandemancilha@outlook.com>
2021-08-10 08:19:34 -0300

    Criando a Classe Person e Instanciando os Objetos personOne e personTwo.

---
+++ ClassAndObjects/Person.cs
@@ -0,0 +1,10 @@
+namespace ClassAndObjects
+{
+    public class Person
+    {
+        //Fields
+        public string FirstName;
+        public int Age;
+        public string Gender;
+    }
+}
---
+++ ClassAndObjects/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person();
+            personOne.FirstName = "Allan";
+            personOne.Age = 23;
+            personOne.Gender = "Male";
+         
+            System.Console.WriteLine($"HELLO, MY NAME IS {personOne.FirstName}, I'M {personOne.Age} YEARS OLD AND MY GENDER IS {personOne.Gender}.");
+
+            Person personTwo = new Person {
+                FirstName = "Priscila",
+                Age = 22,
+                Gender =  "Female"
+            };
+
+            System.Console.WriteLine($"HELLO, MY NAME IS {personTwo.FirstName}, I'M {personTwo.Age} YEARS OLD AND MY GENDER IS {personTwo.Gender}.");
+
+            Console.ReadLine();
+        }
+    }
+}
