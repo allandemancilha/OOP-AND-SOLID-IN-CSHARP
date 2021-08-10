33e439c7d50e568e1ccf3bb2acc7e3d63d35139c
allandemancilha <allandemancilha@outlook.com>
2021-08-10 08:34:57 -0300

    Adicionando o Método Introduce() na Classe Person.

--- ClassAndObjects/Person.cs
+++ ClassAndObjects/Person.cs
@@ -6,5 +6,11 @@ namespace ClassAndObjects
         public string FirstName;
         public int Age;
         public string Gender;
+
+        public void Introduce ()
+        {
+            System.Console.WriteLine($"HELLO, MY NAME IS {FirstName}, I'M {Age} YEARS OLD AND MY GENDER IS {Gender}.");
+        }
+
     }
 }
--- ClassAndObjects/Program.cs
+++ ClassAndObjects/Program.cs
@@ -10,16 +10,16 @@ namespace ClassAndObjects
             personOne.FirstName = "Allan";
             personOne.Age = 23;
             personOne.Gender = "Male";
-         
-            System.Console.WriteLine($"HELLO, MY NAME IS {personOne.FirstName}, I'M {personOne.Age} YEARS OLD AND MY GENDER IS {personOne.Gender}.");
 
+            personOne.Introduce();
+         
             Person personTwo = new Person {
                 FirstName = "Priscila",
                 Age = 22,
                 Gender =  "Female"
             };
-
-            System.Console.WriteLine($"HELLO, MY NAME IS {personTwo.FirstName}, I'M {personTwo.Age} YEARS OLD AND MY GENDER IS {personTwo.Gender}.");
+            
+            personTwo.Introduce();
 
             Console.ReadLine();
         }
