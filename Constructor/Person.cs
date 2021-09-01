6764227015140907efb70ef48b5b76f8754c48f5
allandemancilha <allandemancilha@outlook.com>
2021-09-01 10:15:13 -0300

    Exemplificando Overloading de Métodos Utilizando o Construtor da Classe Person.

--- Constructor/Person.cs
+++ Constructor/Person.cs
@@ -7,9 +7,17 @@ namespace Constructor
         public int Age;
         public string Gender;
 
+        /* SE NENHUM VALOR FOR ATRIBUÍDO QUANDO O OBJETO FOR INSTANCIADO, O VA-
+           LOR PASSADO SERÁ O VALOR PADRÃO DE CADA TIPO, EXEMPLO:
+           
+           String = null
+           Int = 0
+
+           HELLO, MY NAME IS _______, I'M 0 YEARS OLD AND MY GENDER IS ________.
+        */
         public Person()
         {
-            
+
         }
 
         public Person(string firstName, int age, string gender)
--- Constructor/Program.cs
+++ Constructor/Program.cs
@@ -7,8 +7,15 @@ namespace Constructor
         static void Main(string[] args)
         {
             Person personOne = new Person();
+            personOne.FirstName = "Allan";
+            personOne.Age = 23;
+            personOne.Gender = "Male";
             
             personOne.Introduce();
+
+            Person personTwo = new Person("Priscila", 22, "Female");
+
+            personTwo.Introduce();
 
             Console.ReadLine();
         }
