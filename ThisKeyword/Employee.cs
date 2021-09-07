524c93bf67b7929f990e477145a335d19e22fabb
allandemancilha <allandemancilha@outlook.com>
2021-09-06 22:10:06 -0300

    Corrigindo Erro CS0116.

--- ThisKeyword/Employee.cs
+++ ThisKeyword/Employee.cs
@@ -4,17 +4,18 @@ namespace ThisKeyword
     {
         public string FirstName { get; set; }
         public string Email { get; set; }
-    }
+    
+        public Employee(string FirstName, string Email)
+        {
+            /* A PALAVRA-CHAVE this REPRESENTA UMA INSTÂNCIA DA CLASSE
+            E DE SEUS USOS É EVITAR AMBIGUIDADES ENTRE OS NOMES DAS   
+            PROPRIEDADES DA CLASSE E DOS PARÂMETROS.     
+            */
+            
+            this.FirstName = FirstName;
+            this.Email = Email;
+        }
 
-    public Employee(string FirstName, string Email)
-    {
-        /* A PALAVRA-CHAVE this REPRESENTA UMA INSTÂNCIA DA CLASSE
-           E DE SEUS USOS É EVITAR AMBIGUIDADES ENTRE OS NOMES DAS   
-           PROPRIEDADES DA CLASSE E DOS PARÂMETROS.     
-        */
-        
-        this.FirstName = FirstName;
-        this.Email = Email;
     }
 
 }
